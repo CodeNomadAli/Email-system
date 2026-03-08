de4959bfad445d2fcbbd3cf5e59cda020feda26b
unknown <axorawebsolutions@gmail.com>
2026-03-08 09:13:33 +0500

    fiux

--- src/app/api/apps/emails/get/route.ts
+++ src/app/api/apps/emails/get/route.ts
@@ -1,4 +1,6 @@
 // app/api/apps/emails/get/route.ts
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from 'next/server'
 import { getAuthUser } from '@/utils/backend-helper'
 import prisma from '@/db'
